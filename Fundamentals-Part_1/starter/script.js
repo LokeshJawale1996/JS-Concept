0dd066e801c235a9e2794bf2a02d9e5a863493f6
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-16 14:12:13 +0530

    initil commit

--- Fundamentals-Part_1/starter/script.js
+++ Fundamentals-Part_1/starter/script.js
@@ -254,30 +254,28 @@ console.log(n);
 
 */
 ///////////////////////////////////////
+//Truthy and Falsy Values
 
-/* //Truthy and Falsy Values
-
-// 5 falsy values: 0, '', undefined, null, NaN
+/* // 5 falsy values: 0, '', undefined, null, NaN
 console.log(Boolean(0));
 console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
+console.log(Boolean("Jonas"));
 console.log(Boolean({}));
-console.log(Boolean(''));
+console.log(Boolean(""));
 
 const money = 100;
 if (money) {
   console.log("Don't spend it all ;)");
 } else {
-  console.log('You should get a job!');
+  console.log("You should get a job!");
 }
 
 let height = 0;
 if (height) {
-  console.log('YAY! Height is defined');
+  console.log("YAY! Height is defined");
 } else {
-  console.log('Height is UNDEFINED');
+  console.log("Height is UNDEFINED");
 }
-
 */
 ////////////////////////////////////
 
