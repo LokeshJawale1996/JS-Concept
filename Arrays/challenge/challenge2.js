7a42a860fb00d2f4005454c9194eddefa582e023
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-09-08 18:55:16 +0530

    challenge 2 solution using method chaining

--- Arrays/challenge/challenge2.js
+++ Arrays/challenge/challenge2.js
@@ -40,3 +40,24 @@ const age1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const age2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(age1, age2); //44 47.333333333333336
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, 
+but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+//calculating average using method chaining
+const calcAverageHumanAge2 = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg3 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+const avg4 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg3, avg4); //44 47.333333333333336
--- Arrays/final/script.js
+++ Arrays/final/script.js
@@ -509,7 +509,8 @@ console.log(totalDepositsUSD);
 // Coding Challenge #3
 
 /* 
-Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, 
+but this time as an arrow function, and using chaining!
 
 TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
