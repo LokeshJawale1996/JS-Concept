8f2b58cdc5abf54b6f2057e04c6faa6f330ddfc8
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-09-08 18:36:52 +0530

    method chaining calcDisplaySummary of bankist app

--- Arrays/bankist/bankist.js
+++ Arrays/bankist/bankist.js
@@ -86,7 +86,28 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance} EUR`;
 };
 
-calcDisplayBalance(account1.movements);
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}`;
+};
+
+calcDisplaySummary(account1.movements);
 
 //Computing usernames
 const createUsernames = function (accs) {
@@ -99,6 +120,6 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
-/////////////////////////////////////////////////
+// console.log(accounts);
+
 /////////////////////////////////////////////////
